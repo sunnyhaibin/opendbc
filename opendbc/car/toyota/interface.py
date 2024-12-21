fa1cf4e017aa8bcb29ffe7e6b09785862a15c348
rav4kumar <meetkumardesai@gmail.com>
2024-12-21 12:50:07 -0700

    stockg

--- opendbc/car/toyota/interface.py
+++ opendbc/car/toyota/interface.py
@@ -151,9 +151,9 @@ class CarInterface(CarInterfaceBase):
         ret.vEgoStarting = 0.05
         ret.stoppingDecelRate = 0.03  # reach stopping target smoothly
       else:
-        ret.vEgoStopping = 0.25
-        ret.vEgoStarting = 0.25
-        ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+        ret.vEgoStopping = 0.15
+        ret.vEgoStarting = 0.05
+        ret.stoppingDecelRate = 0.1  # reach stopping target smoothly
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
